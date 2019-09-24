632ee51bb9a1f9e4b13935ede0517ba0e7bf42f0
wangtangyun <wangtangyun@onething.net>
2019-09-24 20:45:48 +0800

    Initial commit

---
+++ app/src/main/java/com/rongxianren/desinwidget/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.rongxianren.desinwidget
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.ViewCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/rongxianren/desinwidget/custombehavior/ContainerActivity.kt
@@ -0,0 +1,13 @@
+package com.rongxianren.desinwidget.custombehavior
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rongxianren.desinwidget.R
+
+class ContainerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_container)
+    }
+}
