fca5dcc93ea355b61a8f71535d08edd8ec3b4f9f
wangtangyun <wangtangyun@onething.net>
2019-09-24 21:00:40 +0800

    添加meterial依赖

--- app/src/main/java/com/rongxianren/desinwidget/MainActivity.kt
+++ app/src/main/java/com/rongxianren/desinwidget/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.rongxianren.desinwidget
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.view.ViewCompat
+import com.rongxianren.desinwidget.custombehavior.ContainerActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        item_1.setOnClickListener {
+            goCustomBehavior()
+        }
+    }
+
+    fun goCustomBehavior() {
+        val intent = Intent(this, ContainerActivity::class.java)
+        startActivity(intent)
     }
 }
