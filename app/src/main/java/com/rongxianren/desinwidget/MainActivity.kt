dc6768648bc314e279966bae4eedf1870319be9d
wangtangyun <wangtangyun@onething.net>
2019-09-25 22:19:10 +0800

    NestedScrolling嵌套滑动机制问题

--- app/src/main/java/com/rongxianren/desinwidget/MainActivity.kt
+++ app/src/main/java/com/rongxianren/desinwidget/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.view.ViewCompat
 import com.rongxianren.desinwidget.custombehavior.ContainerActivity
+import com.rongxianren.desinwidget.nestedscrolling.NestedScrollingActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,10 +16,19 @@ class MainActivity : AppCompatActivity() {
         item_1.setOnClickListener {
             goCustomBehavior()
         }
+
+        item_2.setOnClickListener {
+            goNestedScrolling()
+        }
     }
 
     fun goCustomBehavior() {
         val intent = Intent(this, ContainerActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun goNestedScrolling() {
+        val intent = Intent(this, NestedScrollingActivity::class.java)
         startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/rongxianren/desinwidget/nestedscrolling/NestedScrollingActivity.kt
@@ -0,0 +1,13 @@
+package com.rongxianren.desinwidget.nestedscrolling
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rongxianren.desinwidget.R
+
+class NestedScrollingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nested_scrolling)
+    }
+}
